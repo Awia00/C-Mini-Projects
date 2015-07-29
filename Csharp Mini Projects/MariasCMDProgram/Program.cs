1995130df04e68f58ba582e0abdef59601970783
Anders Wind <wind@airtame.com>
2015-07-29 23:36:44 +0200

    Tilføjede marias projekt

--- Csharp Mini Projects/MariasCMDProgram/Program.cs
+++ Csharp Mini Projects/MariasCMDProgram/Program.cs
@@ -10,6 +10,47 @@ namespace MariasCMDProgram
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello what is your name?");
+            string input= Console.ReadLine();
+            Console.WriteLine("hello " + input); 
+            Console.WriteLine("is maria the coolest person in the world?");
+            string input1 = Console.ReadLine();
+            if (input1 == "yes")
+            {
+                Console.WriteLine("well " + input);
+                Console.WriteLine("you are completly rigth!");
+                Console.WriteLine("please give her a kiss");
+
+                for (int tal = 1; tal <= 10; tal = tal + 1)
+                {
+                    //Console.WriteLine(tal);
+                    Console.WriteLine(input + " Elsker Maria!");
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("wrong anwser "+input);
+                Console.WriteLine("please do not give her a kiss");
+            }
+            
+            Metode1();
+            Metode2();
+
+            
+
+            Console.ReadKey();
         }
+
+        static void Metode1()
+        {
+            Console.WriteLine("Det her kaldes fra metode 1");
+        }
+        static void Metode2()
+        {
+            Console.WriteLine("Det her kaldes fra metode 2");
+        }
+
+
     }
 }
