7f0ccc4cd627d4460e89bc9c00ffc12b108ed5ce
Anders Wind <awia00@gmail.com>
2016-02-15 18:47:54 +0100

    Trying to get a website up and running :)

--- Csharp Mini Projects/EmptyAspNet5/Startup.cs
+++ Csharp Mini Projects/EmptyAspNet5/Startup.cs
@@ -15,16 +15,22 @@ namespace EmptyAspNet5
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app)
         {
+            app.UseMvc();
             app.UseIISPlatformHandler();
 
-            app.Run(async (context) =>
+            app.UseMvc(r =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                r.MapRoute(
+                    name: "default", 
+                    template: "{controller}/{action}/{id?}", 
+                    defaults: new { controller = "Home", action = "Index"});
+
             });
         }
 
