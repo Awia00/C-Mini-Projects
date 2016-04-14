30c0b59f0b38f400621ae8e0c885e59878fdaa68
Anders Wind <awia00@gmail.com>
2016-04-15 00:33:20 +0200

    Working on an introduction screen for the spotlightgetter

---
+++ SpotlightGetterSolution/SpotlightGetterSolution/SpotlightGetterMinimal/ViewModels/IntroductionPageViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpotlightGetterMinimal.ViewModels
+{
+    public class IntroductionPageViewModel
+    {
+        #region Actions
+
+        public async void PickSpotlightFolder()
+        {
+            var folderPicker = new Windows.Storage.Pickers.FolderPicker
+            {
+                SuggestedStartLocation = Windows.Storage.Pickers.PickerLocationId.DocumentsLibrary
+            };
+            folderPicker.FileTypeFilter.Add(".txt");
+
+            Windows.Storage.StorageFolder folder = await folderPicker.PickSingleFolderAsync();
+            if (folder != null && folder.Path.Contains(@"Packages\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\LocalState\Assets"))
+            {
+                // Application now has read/write access to all contents in the picked folder
+                // (including other sub-folder contents)
+                Windows.Storage.AccessCache.StorageApplicationPermissions.
+                FutureAccessList.AddOrReplace("SpotlightFolder", folder);
+            }
+        }
+        #endregion Actions
+    }
+}
