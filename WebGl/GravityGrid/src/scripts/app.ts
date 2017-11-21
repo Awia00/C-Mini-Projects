5eb9bc799053ad92df44f181154668721585e92f
Anders Wind <awia00@gmail.com>
2017-11-21 20:44:41 +0100

    MIgrated the rest of Grid to GravityGrid

--- WebGl/GravityGrid/src/scripts/app.ts
+++ WebGl/GravityGrid/src/scripts/app.ts
@@ -1,10 +1,12 @@
 import '../styles/base.scss';
 
 import { Greeter } from './greeter';
+import { IRenderable, Renderer } from './renderer';
 
 const greeter: Greeter = new Greeter('gravity-grid');
+const renderer : IRenderable = new Renderer();
 
-const el = document.getElementById('greeting');
+const el = document.getElementById('grid');
 if (el) {
-  el.innerHTML = greeter.greet();
+  renderer.renderOnCanvas(el);
 }
