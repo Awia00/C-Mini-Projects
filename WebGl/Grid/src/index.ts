a9db175ff32897991957d2bdb80f92898dd5508a
Anders Wind <awia00@gmail.com>
2017-11-25 22:19:58 +0100

    Removed old grid project

--- WebGl/Grid/src/index.ts
+++
@@ -1,6 +0,0 @@
-// index.
-import { Renderer, IRenderable } from "./renderer";
-import * as $ from "jquery";
-
-let renderer:IRenderable = new Renderer();
-renderer.renderOnCanvas(<HTMLElement> document.getElementById("grid"));
