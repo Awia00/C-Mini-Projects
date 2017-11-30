2e250f0b9823cb0940553b6cac94d48e4fb3188f
Anders Wind <awia00@gmail.com>
2017-11-30 10:54:12 +0100

    Removed anderswind.dk specifics from gravityGrid project

--- WebGl/GravityGrid/src/scripts/glsl/2d-fragment-shader.glsl
+++ WebGl/GravityGrid/src/scripts/glsl/2d-fragment-shader.glsl
@@ -10,8 +10,8 @@ uniform vec2 pitch; // number of grids
 uniform vec3 presses[6];
 
 vec4 grid(float modX, float modY, float strength) {
-	float density = 0.4 * (1.0 - abs((2.0-min(modX, modY))/2.0)); // grid alpa 0.5 and then anti alias.
-	return vec4(0.0, 0.0, 0.0, density*strength);
+	float density = 0.4 * (1.0 - abs((2.0-min(modX, modY))/2.0)); // grid alpa 0.4 and then anti alias.
+	return vec4(1.0-density*strength, 1.0-density*strength, 1.0-density*strength, 1.0);
 }
 
 void main() {
@@ -19,7 +19,7 @@ void main() {
 
     vec2 pull = vec2(0.0, 0.0);
     float strength = 0.15;
-    for(int i = 0; i<=6; i++){
+    for(int i = 0; i<=6; i++) {
         if (amtPresses==i)
             break;
         vec3 press = presses[i];
@@ -27,7 +27,7 @@ void main() {
         float distance = length(delta);
         pull += delta * (gravity*press.z / (distance*distance + reach));
         
-        float fade = max(0.0, 0.9-distance/resLength); // 1.2 due to some scaling
+        float fade = max(0.0, 0.9-distance/resLength);
         float dist = min(1.0, max(0.0, press.z));
         strength = max(strength, fade*dist);
     }
@@ -37,5 +37,5 @@ void main() {
     float modX = mod(reach + gl_FragCoord.x, newPos.x);
     float modY = mod(reach + gl_FragCoord.y, newPos.y);
 
-	gl_FragColor = grid(modX, modY, strength);
+	gl_FragColor = grid(modX, modY, strength);// + vec4(0.1, 0.1, 0.1, 1.0);
 }
